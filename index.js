6fa8c103898ad83802b17ffa20f561cb1a11cea3
usman <usman@pop-os.localdomain>
2020-09-19 12:40:45 +0500

    dfasf

--- index.js
+++ index.js
@@ -106,7 +106,7 @@ app.get('/api/v1/public',(req,res)=>{
 
 console.log(process.env.NODE_ENV);
 
-if (process.env.NODE_ENV !== 'production') {
+if (process.env.NODE_ENV === 'production') {
     // Express will serve up production assets
     // like our main.js file, or main.css file!
     app.use(express.static('client/build'));
